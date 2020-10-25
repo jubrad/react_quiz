9f15f2cb8fc69c0acd27cf02b1cee515a2349a87
daxoliveira <dax-oliveira-br@gmail.com>
2020-10-25 15:36:38 -0500

    Clean up app.js to use react router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
+import React from 'react';
 import styled from 'styled-components';
-import PlayerModal from './components/PlayerModal';
+import { BrowserRouter, Route } from 'react-router-dom';
+import GameStart from './views/GameStart';
 
 const AppCard = styled.div`
   text-align: center;
@@ -15,104 +16,18 @@ const AppCardHeader = styled.header`
   font-size: calc(10px + 2vmin);
   color: var(--font-color-primary);
 `
-const AppCardMain = styled.main`
-  background-color: var(--color-primary-light);
-  min-height: 84.75vh;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  font-size: calc(10px + 2vmin);
-  color: var(--font-color-primary);
-  `
-const AppCardButton = styled.button`
-  background-color: var(--color-secondary-dark);
-  min-height: 8vh;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  font-size: calc(10px + 2vmin);
-  color: var(--font-color-secondary);
-  border-radius: 2vh;
-`
-const Form = styled.form`
-  background-color: var(--color-primary-light);
-  min-height: 10vh;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  font-size: calc(10px + 2vmin);
-  color: var(--font-color-secondary);
-`
+function App() {
+  return (
+    <AppCard>
+      <AppCardHeader>
+        <h1>Questions And Answers Game</h1>
+      </AppCardHeader>
 
-class App extends Component {
-  constructor (props) {
-    super(props)
-
-    this.state = {
-      playerName: '',
-      loggedIn: false,
-      show: false
-    }
-  }
-
-  showModal = () => {
-    this.setState({ show: true });
-  };
-
-  hideModal = () => {
-    this.setState({ show: false });
-  };
-
-  handleSubmit = (e) => {
-    e.preventDefault()
-    localStorage.setItem('playerName', this.state.playerName)
-    this.hideModal()
-    this.props.history.push('/questions')
-  }
-
-  handleChange = (e) => {
-    this.setState({playerName: e.target.value})
-  }
-
-  render () {
-    return (
-      <AppCard>
-
-        <AppCardHeader>
-          <h1>Questions And Answers Game</h1>
-        </AppCardHeader>
-
-        <AppCardMain>
-          <p>
-            Click below and type your name to start playing
-          </p>
-
-          <AppCardButton type="button" onClick={this.showModal}>
-            Start Playing
-          </AppCardButton>
-
-        </AppCardMain>
-
-        <PlayerModal show={this.state.show} handleClose={this.hideModal}>
-          <Form onSubmit={this.handleSubmit}>
-            <label>
-              Name of the Player
-              <br/>
-              <input type="text" name="playerName" placeholder="John Doe" onChange={this.handleChange} value={this.state.playerName}/>
-            </label>
-            <br/>
-            <AppCardButton type="submit">
-            Submit and Play!
-            </AppCardButton>
-          </Form>
-        </PlayerModal>
-
-      </AppCard>
-    )
-  }
+      <BrowserRouter>
+          <Route path="/" exact component={GameStart} />
+      </BrowserRouter>
+    </AppCard>
+  );
 }
 
 export default App;
